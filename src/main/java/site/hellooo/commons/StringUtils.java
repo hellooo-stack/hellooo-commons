0132d0691575196c799799112692e952ffce5420
Jeb.Wang <jeb.wannng@gmail.com>
2023-02-03 12:01:34 +0800

    Create StringUtils.java

---
+++ src/main/java/site/hellooo/commons/StringUtils.java
@@ -0,0 +1,13 @@
+package site.hellooo.commons;
+
+/**
+ * @author Jeb.Wang
+ */
+public class StringUtils {
+
+    public static final String EMPTY_STRING = "";
+
+    public static String empty() {
+        return EMPTY_STRING;
+    }
+}
