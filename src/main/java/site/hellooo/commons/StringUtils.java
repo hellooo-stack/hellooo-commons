2bfb5881c49d459b32c91a3a7b5b8f32863d6362
Jeb.Wang <jeb.wannng@gmail.com>
2023-02-07 14:00:51 +0800

    Update StringUtils.java

--- src/main/java/site/hellooo/commons/StringUtils.java
+++ src/main/java/site/hellooo/commons/StringUtils.java
@@ -10,4 +10,20 @@ public class StringUtils {
     public static String empty() {
         return EMPTY_STRING;
     }
+
+    public static boolean isEmpty(CharSequence charSequence) {
+        return charSequence == null || charSequence.length() == 0;
+    }
+
+    public static boolean isEmpty(String string) {
+        return string == null || string.length() == 0;
+    }
+
+    public static boolean isNotEmpty(CharSequence charSequence) {
+        return !isEmpty(charSequence);
+    }
+
+    public static boolean isNotEmpty(String string) {
+        return !isEmpty(string);
+    }
 }
